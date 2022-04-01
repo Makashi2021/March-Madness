9dc170a2dabce452725785a5c587b0ea74dcc5f4
Makashi <hg52254@gmail.com>
2022-03-31 20:14:03 -0600

    Edited run command so it can hosted on GC

--- server.py
+++ server.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 
 
 
+
 @app.route("/basketball/<team1>&<team2>")
 def hello_world(team1, team2):
     return jsonify(winner.findWinner(team1, team2))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()#(debug=False,host='0.0.0.0')
